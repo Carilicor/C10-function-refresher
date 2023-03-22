155991356ced1d74ab415e60351f517f37f596ce
Cari Licor <Carilicor@gmail.com>
2023-03-21 21:14:25 -0400

    functions

---
+++ function.js
@@ -0,0 +1,20 @@
+//Function Declaration 
+function message(firstName) {
+    console.log("Hello " + firstName);
+}
+
+function getMeow() {
+    console.log("meow");
+}
+
+function getSumTotal(firstNumber, secondNumber) {
+    const result = firstNumber + secondNumber;
+    console.log(result);
+}
+
+//invoke a function 
+message("Cari");
+message("cat who are you");
+getMeow();
+getSumTotal(2,5)
+getSumTotal("Happy", "day")
